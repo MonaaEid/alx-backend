e8bbc03b73ce83d1ece54f0d3513b94a8ba55006
MonaaEid <monaa_eid@hotmail.com>
2024-05-04 22:07:22 +0300

    Add readme files

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""function should return a tuple of size two containing a 
+start index and an end index corresponding to the range of indexes 
+to return in a list for those particular pagination parameters.
+Page numbers are 1-indexed, i.e. the first page is page 1."""
+def index_range():
+    """return a tuple of size two containing a start index and an end index"""
+    return (0, 0)
