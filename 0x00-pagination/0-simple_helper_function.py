331c1c6f9a6f1be76f3dbe6f5c78a12acc16a724
MonaaEid <monaa_eid@hotmail.com>
2024-05-06 22:06:25 +0300

    Add readme files

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,6 +3,10 @@
 start index and an end index corresponding to the range of indexes 
 to return in a list for those particular pagination parameters.
 Page numbers are 1-indexed, i.e. the first page is page 1."""
-def index_range():
-    """return a tuple of size two containing a start index and an end index"""
-    return (0, 0)
+
+
+def index_range(page, page_size):
+    """comment"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
