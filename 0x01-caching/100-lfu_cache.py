7f2713a701a6f677f038927d3461d34d5a7ff927
MonaaEid <monaa_eid@hotmail.com>
2024-05-08 01:08:48 +0300

    class LFU cache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -13,26 +13,31 @@ class LFUCache(BaseCaching):
         """
         super().__init__()
         self.queue = []
-
+        self.frequency = {}
+    
     def put(self, key, item):
         """ Add an item in the cache
         """
         if key and item:
             if key in self.cache_data:
                 self.cache_data[key] = item
+                self.frequency[key] += 1
                 self.queue.remove(key)
             else:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                     discard = self.queue.pop(0)
                     del self.cache_data[discard]
+                    del self.frequency[discard]
                     print("DISCARD:", discard)
                 self.cache_data[key] = item
+                self.frequency[key] = 1
             self.queue.append(key)
 
     def get(self, key):
         """ Get an item by key
         """
         if key in self.cache_data:
+            self.frequency[key] += 1
             self.queue.remove(key)
             self.queue.append(key)
             return self.cache_data[key]
