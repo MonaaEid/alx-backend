2fffa4fb8fea7685bbb41228bbddcfc959cd0395
MonaaEid <monaa_eid@hotmail.com>
2024-05-15 00:31:35 +0300

    Add the tasks files

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""instantiate the Babel object in your app.
+Store it in a module-level variable named babel"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Config class"""
+    LANGUAGES = ['en', 'fr']
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello():
+    """Return 0-index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
