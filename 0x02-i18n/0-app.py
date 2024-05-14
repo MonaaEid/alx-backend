c5a99dd89774a8ecf5d29e88a6e3b4f125afe384
MonaaEid <monaa_eid@hotmail.com>
2024-05-14 23:20:29 +0300

    Add the tasks files

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,10 +2,14 @@
 """Welcome to Holberton"""
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
     """Return 0-index.html"""
     return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
