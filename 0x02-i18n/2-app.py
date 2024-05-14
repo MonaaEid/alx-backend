6c5aa641e67d2de0d55b7460754bc03fae1c3cd1
MonaaEid <monaa_eid@hotmail.com>
2024-05-15 01:20:56 +0300

    Add the tasks files

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -11,6 +11,8 @@ babel = Babel(app)
 class Config:
     """Config class"""
     LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
