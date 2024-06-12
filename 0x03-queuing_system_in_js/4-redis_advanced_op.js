a85b0aca29625e7fdcb10606066aef8c3ac3474b
MonaaEid <monaa_eid@hotmail.com>
2024-06-12 23:48:18 +0300

    test

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    print('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    print(`Redis client not connected to the server: ${err}`);
+});
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+
+client.hgetall('HolbertonSchools', (err, result) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(result);
+    }
+});
+
